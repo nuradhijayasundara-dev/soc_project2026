825e7cb76e42d0ace26ac5c88709c8bf9d804b6f
Wiyathma <nuradhijayasundara@gmail.com>
2026-07-19 15:32:26 +0530

    deleted files

--- courier-service/src/main/java/com/backhaulmatch/courier/repository/ReceiverRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backhaulmatch.courier.repository;
-
-import com.backhaulmatch.courier.entity.Receiver;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReceiverRepository extends JpaRepository<Receiver, Long> {
-}
