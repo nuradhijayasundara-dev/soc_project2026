b9267b4d1897c3c909ff3b5e6b87d1c52202a3ca
milan-cyromancer <milandemoshan@gmail.com>
2026-09-14 10:59:15 +0530

    Update frontend1

--- courier-portal/src/api/customerApi.js
+++
@@ -1,6 +0,0 @@
-import apiClient from './apiClient';
-
-export const getCustomers = () => apiClient.get('/courier/customers').then((r) => r.data);
-
-export const createCustomer = (payload) =>
-  apiClient.post('/courier/customers', payload).then((r) => r.data);
